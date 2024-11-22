3a3ce27e76b726bb0322a24cedb2361a0957a25b
Yago Baamonde <9005709@ALUMNOS.UFV.ES>
2024-11-22 11:16:55 +0100

    Eliminación total lógica Notificaciones Push - Demasiado complejo para el alcance del proyecto

--- backend/nextstepBackEnd/src/main/java/com/nextstep/nextstepBackEnd/NextstepBackEndApplication.java
+++ backend/nextstepBackEnd/src/main/java/com/nextstep/nextstepBackEnd/NextstepBackEndApplication.java
@@ -1,7 +1,15 @@
 package com.nextstep.nextstepBackEnd;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class NextstepBackEndApplication {
