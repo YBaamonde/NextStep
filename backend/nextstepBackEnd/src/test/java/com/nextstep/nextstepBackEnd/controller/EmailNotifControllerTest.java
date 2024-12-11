2e45634dcbcfcfa80b153fc2d639477e75d8fea3
Yago Baamonde <9005709@ALUMNOS.UFV.ES>
2024-12-11 22:10:28 +0100

    Fin rama Develop

--- backend/nextstepBackEnd/src/test/java/com/nextstep/nextstepBackEnd/controller/EmailNotifControllerTest.java
+++ backend/nextstepBackEnd/src/test/java/com/nextstep/nextstepBackEnd/controller/EmailNotifControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class EmailNotifControllerTest {
 
