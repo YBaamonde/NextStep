6f807645cde59c028a298eb23e0acd78e5b2fe2b
Yago Baamonde <9005709@ALUMNOS.UFV.ES>
2024-12-11 22:13:43 +0100

    Fin rama Develop - infinito

--- backend/nextstepBackEnd/src/test/java/com/nextstep/nextstepBackEnd/NextstepBackEndApplicationTests.java
+++ backend/nextstepBackEnd/src/test/java/com/nextstep/nextstepBackEnd/NextstepBackEndApplicationTests.java
@@ -1,9 +1,12 @@
 package com.nextstep.nextstepBackEnd;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NextstepBackEndApplicationTests {
 
 	@Test
